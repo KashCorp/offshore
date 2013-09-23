472f619eb0561d4bb43b3d9a4c2b1402c566f055
Mike Robbins <m4robb@gmail.com>
2013-09-23 18:07:31 -0400

    credits

---
+++ overlay/credits.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+<meta name="viewport" content="width = 1050, user-scalable = no" />
+<link rel="stylesheet" type="text/css" href="../css/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+<!-- JavaScripts -->
+<script type="text/javascript" src="../js/lib/jquery.min.js"></script>
+<script type="text/javascript" src="../js/lib/jquery-ui.min.js"></script>
+</head>
+<body style="background:none; overflow-y: auto;">
+
+
+
+<div style="font-size: 1.5em;padding-top:10%;width:55%;margin:auto; overlay:auto">
+
+
+OFFSHORE CREDITS 
+
+
+DIRECTORS <br>
+Brenda Longfellow, Glen Richards and Helios Design Lab. 
+<br><br>
+PRODUCER <br>
+Brenda Longfellow
+<br><br>
+INTERACTIVE DESIGN AND DEVELOPMENT
+Producer: Sarah Arruda <br>
+Interactive/Technical Director: Mike Robbins <br>
+Creative Director: Alex Wittholz <br>
+Graphic Design: Felix Wittholz, Joanna Durkalec <br>
+Interactive Programming: Mike Robbins, Iain Campbell, Daniel Sundy <br>
+3D Programming : Matt Brushett, Marc Pannozzo<br>
+<br>
+CINEMATOGRAPHERS <br>
+Louisiana: Edward Holub, Glen Richards<br>
+Boston: Jodi Silver.<br>
+Houston: Boyderick Mays, Trent Wittenbach <br>
+Barrow: Dustinn Craig   <br>            
+<br>
+SOUND DESIGN WEBSITE <br>
+John Black/ Cypheraudio<br>
+<br>
+MUSIC COMPOSITION<br>
+Antoni Komasa-Łazarkiewicz<br>
+<br>
+DOCUMENTARY EDITORS<br>
+Glen Richards, Cliff Caines, Tim Kirkwood, Chris Wiseman<br>
+<br>
+SOUND EDIT<br>
+Alex Unger<br>
+<br>
+DOCUMENTARY PRODUCTION CO-ORDINATORS <br>
+Barrow: Deborah Dahl Edwardson<br>
+Ghana: Sedem Ofori<br>
+Brazil: Ricardo Salles de Sá <br>
+<br>
+VOICE OVER<br>
+Ines Buchli<br>
+<br>
+CONSULTANTS<br>
+Anna Zalik<br>
+James Marriott<br>
+John Jordan<br>
+Imre Szeman<br>
+Joseph Johnson Cami<br>
+<br>
+RESEARCH ASSISTANTS<br>
+Sama Waham<br>
+Daniel Valle<br>
+<br>
+Helicopter<br>
+Dacoda Bartel<br>
+<br>
+THANK YOU<br>
+Town of Barrow<br>
+Darla and Todd Rooks<br>
+Kindra and David Arnesen<br>
+Tim «Blimp» Cheramie<br>
+Byron Encalade<br>
+Rosina Philippe<br>
+Dean Blanchard<br>
+Shane Bagala<br>
+Brad Robin <br>
+Keith Jones<br>
+Rolake Akinkube<br>
+Scott Porter<br>
+Susan Felio and Butch Price<br>
+Margaret and Kevin Curole<br>
+Gulf Restoration Network<br>
+Aaron Viles<br>
+Jonathan Henderson<br>
+Eco Rigs<br>
+Michael Klare<br>
+Loren Steffy<br>
+Tony Buzbee<br>
+Catfish Miller<br>
+Jorey Danos<br>
+Jolene Plaisance<br>
+Dr. Andew Whitehead<br>
+Dr. Mike Robichaux<br>
+Mr. Jack<br>
+Mr. Floyd<br>
+Byron King<br>
+Robert Amsterdam<br>
+Mark Peters<br>
+Antony Goldman<br>
+Louisiana Bucket Brigade<br>
+Anne Rolfes<br>
+John Donovan<br>
+The Alabama Oil Spill Aftermath Coalition<br>
+Kimberly McCuiston<br>
+Michele Walker-Harmon<br>
+Captain Lori<br>
+Mike Arceneaux<br>
+Don Van Nieuwenhuise<br> 
+Bernardo Batista<br>
+Alex Leopol<br>
+Dan Epstein<br>
+<br>
+Funded by a SSHRC Research and Creation Grant<br>
+York University<br>
+
+
+</div>
+
+<a id="close-control" class="platform-nav" style="right:25px">Close</a>
+
+
+<script type="text/javascript">
+
+  $(".platform-nav").click(function(){
+    parent.master.closeOverlay()
+  })
+
+
+</script>
+
+</body>
+</html>
