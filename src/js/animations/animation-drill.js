c7345b68f7495b577866ffd47bb41d1e745989bb
m4robb <mike@heliozilla.com>
2015-08-15 11:03:35 -0400

    at home

---
+++ src/js/animations/animation-drill.js
@@ -0,0 +1,62 @@
+// animation for the circling helicopter in the prologue
+var THREEANIM = {
+
+	customVars: {
+		angle:0,
+		posX1:null,
+		posZ1: null,
+		r: 500
+	},
+
+	objInit: function(_mesh,_camera,_scene){
+
+		//audiomaster.loadAudio( '../audio/chopper','chopper',.01,-1)
+
+
+		_mesh.scale.set(-.5, -.5, .5);
+		_mesh.position.set(100 * Math.cos(180 * Math.PI / 180), 100, 100 * Math.sin(180 * Math.PI / 180))
+		_mesh.rotation.set(0, 0, 90 * Math.PI / 180)
+
+		console.log(_mesh)
+
+		var directionalLight = new THREE.DirectionalLight(0x0000ff);
+		directionalLight.position.x = 100;
+		directionalLight.position.y = -700;
+		directionalLight.position.z = -1;
+		directionalLight.position.normalize();
+		_scene.add( directionalLight );
+		THREEANIM.mesh = _mesh;
+		THREEANIM.camera = _camera
+	},
+
+
+	objKill: function(){
+		console.log('farewell')
+		// audiomaster.mix.getTrack('chopper').gain(0)
+		// audiomaster.mix.removeTrack('chopper')
+	},
+
+	objAnim: function(){
+
+		 THREEANIM.customVars.r -= 0.01
+
+			// if(THREEANIM.customVars.angle < 360){
+			// 	THREEANIM.customVars.angle += .1
+			// } else{
+			// 	THREEANIM.customVars.angle = 0
+			// }
+
+			// if (THREEANIM.mesh)
+			// {
+
+			// 	THREEANIM.customVars.posX1 = THREEANIM.customVars.r * Math.cos(THREEANIM.customVars.angle * Math.PI / 180)
+	  //   		THREEANIM.customVars.posZ1 = THREEANIM.customVars.r * Math.sin(THREEANIM.customVars.angle * Math.PI / 180)
+	  //   		THREEANIM.mesh.lookAt( THREEANIM.camera.position)
+
+	  //   		audiomaster.mix.getTrack('chopper').pan3d(THREEANIM.customVars.posX1 * .01,THREEANIM.customVars.posZ1* .01)
+	  //   		THREEANIM.mesh.position.set(THREEANIM.customVars.posX1, -100,THREEANIM.customVars.posZ1)
+			// }
+
+	}
+
+}
